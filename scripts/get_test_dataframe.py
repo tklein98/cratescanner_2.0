3bc3af0afd10e272c160e403be98fca10e8809aa
Tobias Klein <tob.klein1@gmail.com>
2021-03-04 09:27:49 +0100

    improve cleaning of labels, now includes numbers and special characters and only deletes the numbers from the file numbering

--- scripts/get_test_dataframe.py
+++ scripts/get_test_dataframe.py
@@ -1,11 +1,10 @@
 from os import listdir
 from os.path import isfile, join
-import matplotlib.pyplot as plt
 import pandas as pd
-from imageio import imread
 
-def get_test_dataframe(directory_path):
+def get_testing_dataframe(directory_path):
     '''directory_path: Your directory path where you have stored all the testing images'''
+    digits = ['0','1','2','3','4','5','6','7','8','9']
 
     # Create list of all filenames
     filenames = [f for f in listdir(directory_path) if isfile(join(directory_path, f))]
@@ -16,20 +15,27 @@ def get_test_dataframe(directory_path):
     # Create Dataframe
     df = pd.DataFrame(columns=["picture_path","filename", "label"])
 
-    # Whitelist for creating cleaned string
-    whitelist = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
     # Creating labeled dataframe with picture filepath, filename and the respective label
-    # Syntax for label: all lowercase no label
     for i in range(len(filenames)):
-        # Clear string of all numbers and special characters and spaces
-        cleaned_string = ''.join(filter(whitelist.__contains__, filenames[i]))
+        # Clear string of spaces, the numbers from the jpeg (not possible album names with numbers) and the underscores
 
-        #Appending each row with image filepath,filename and its label
+        # remove .jpg and lowercase
+        cleaned_string = filenames[i][:-4].lower()
+        # Delete the numbers from the scraping naming convention, do this twice as there are can be two digit numbers
+        if cleaned_string[-1] in digits:
+            cleaned_string = cleaned_string[:-1]
+
+        if cleaned_string[-1] in digits:
+            cleaned_string = cleaned_string[:-1]
+
+        # remove whitespace and underscores
+        cleaned_string = cleaned_string.replace(" ", "").replace('_','')
+
+        # Appending each row with image filepath,filename and its label
         df = df.append({
              "picture_path": f'{directory_path}{filenames[i]}',
              "filename": filenames[i],
              # Clear the remaining 'jpg' string at the end of every cleaned string
-             "label":  cleaned_string[:-3].lower()
+             "label": cleaned_string
               }, ignore_index=True)
     return df
