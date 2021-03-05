89f1dd2586f0b524ae41c8975404496fdc297d84
Marie Chauvois <mrchvsghn@gmail.com>
2021-03-05 16:42:47 +0100

    added condition to review scraper

--- crate_scanner/scrapers/reviews_scraper.py
+++ crate_scanner/scrapers/reviews_scraper.py
@@ -35,49 +35,55 @@ def get_top3_reviews(artist, album):
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, features="lxml")
 
-    #locate correct album and prepare URL to user reviews
-    albums = soup.find_all("li", class_="album")
+     # check if results
 
-    # keep only albums with the right artist
-    artists = []
-    for element in albums:
-        a_list = []
-        if element.find("div", class_="artist"):
-            tag = element.find("div", class_="artist")
-            if tag.find("a"):
-                a_list.append(tag)
+    if soup.find("ul", class_="search-results"):
+        #locate correct album and prepare URL to user reviews
+        albums = soup.find_all("li", class_="album")
 
-                for a in a_list:
-                    tag = a.find("a")
-                    if tag.text.lower() == artist:
-                        artists.append(element)
+        # keep only albums with the right artist
+        artists = []
+        for element in albums:
+            a_list = []
+            if element.find("div", class_="artist"):
+                tag = element.find("div", class_="artist")
+                if tag.find("a"):
+                    a_list.append(tag)
 
-    #keep only albums with the right album name, and retrieve url
+                    for a in a_list:
+                        tag = a.find("a")
+                        if tag.text.lower() == artist:
+                            artists.append(element)
 
-    album_list = []
-    for element in artists:
-        tag = element.find("div", class_="title").find("a")
-        if tag.text.lower() == album:
-            album_list.append(tag['href'])
+        #keep only albums with the right album name, and retrieve url
+        album_list = []
+        for element in artists:
+            tag = element.find("div", class_="title").find("a")
+            if tag.text.lower() == album:
+                album_list.append(tag['href'])
 
-    url_2 = album_list[0]
+        if album_list == []:
+            return False
 
-    #build url to the reviews page, and retrieve page
-    url_reviews = f"{url_2}/user-reviews"
-    driver.get(url_reviews)
-    page_source_2 = driver.page_source
-    soup = BeautifulSoup(page_source_2, features="lxml")
+        url_2 = album_list[0]
 
-    #extract reviews into a list
-    reviews = []
-    for review in soup.find_all("div", class_="user_review"):
-        review_div = review.find("div", class_="middle")
-        reviews.append(review_div.text)
+        #build url to the reviews page, and retrieve page
+        url_reviews = f"{url_2}/user-reviews"
+        driver.get(url_reviews)
+        page_source_2 = driver.page_source
+        soup = BeautifulSoup(page_source_2, features="lxml")
 
-    # display message if no reviews were found
-    if reviews == []:
-        return False
+        #extract reviews into a list
+        reviews = []
+        for review in soup.find_all("div", class_="user_review"):
+            review_div = review.find("div", class_="middle")
+            reviews.append(review_div.text)
 
-    #display top 3 reviews
-    return reviews[:2]
+        # display message if no reviews were found
+        if reviews == []:
+            return False
 
+        #display top 3 reviews
+        return reviews[:2]
+
+    return False
